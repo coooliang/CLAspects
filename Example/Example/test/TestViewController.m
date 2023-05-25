a1b7a0a77cdc66bc40d461e78947aa2499b6b0ff
coooliang <wot_chenliang_xdl@cibdev.com>
2023-05-25 11:11:48 +0800

    updatee

--- Example/Example/test/TestViewController.m
+++ Example/Example/test/TestViewController.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.title = @"TestViewController";
     self.channel = @"channel 123";
     // Do any additional setup after loading the view.
 }
